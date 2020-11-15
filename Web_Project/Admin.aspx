8b6f1be4ffd2f8f74c60ad5e93a84fbe94defb40
NaheelSalman <39267623+NaheelSalman@users.noreply.github.com>
2020-11-15 18:49:37 +0500

    Developers Community

---
+++ Web_Project/Admin.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Web_Project.Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="Scripts/bootstrap.min.css"/>
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" />
+     <script src="Scripts/jquery.min.js"></script>
+  <script src="Scripts/popper.min.js"></script>
+  <script src="Scripts/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1 style="text-align:center;"><asp:Label ID="lblgridmsg" runat="server" CssClass="text-center" Text="Welcome To Admin Page"></asp:Label></h1>
+        <asp:GridView ID="txtGridView" CssClass="table table-dark table-sm" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Web_Project/Admin.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Project
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AutoUpdateGridView();
+        }
+
+        private void AutoUpdateGridView()
+        {
+            try 
+            {
+                using (var db = new CommunitySystemDataContext())
+                {
+                    var us = from uss in db.Members
+                             select uss;
+                    txtGridView.DataSource = us;
+                    txtGridView.DataBind();
+                };
+            } catch (Exception ex) 
+                {
+                lblgridmsg.Text = ex.Message;    
+                }
+            
+        }
+    }
+}
